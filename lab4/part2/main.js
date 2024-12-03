fd651a0b02e76dc72a851ff3b8a18d31aaf36a27
Tubtonic <35375707+Tubtonic@users.noreply.github.com>
2024-12-02 19:47:52 -0500

    Added comments and created array for images

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -1,13 +1,24 @@
+//Name: Toby Pike
+//File: main.js
+//Date: November 23, 2024
+//main.js for assignment 4 part 2
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
-
+/* Declares the array of image file names */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+/* Declares the alternative text for each image file */
+const altText = [
+    'Description for pic1',
+    'Description for pic2',
+    'Description for pic3',
+    'Description for pic4',
+    'Description for pic5',
+  ];
 /* Looping through images */
 
 const newImage = document.createElement('img');
